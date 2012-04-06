9efdb2071d71a72b3849627e4430b8d17be35507
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2012-04-06 11:50:08 +0200

    Add a logger to display message

--- src/main/groovy/org/abnt/stripes/generator/Runner.groovy
+++ src/main/groovy/org/abnt/stripes/generator/Runner.groovy
@@ -1,20 +1,37 @@
 package org.abnt.stripes.generator
 
 import org.abnt.stripes.generator.utils.Asker
+import org.abnt.stripes.generator.utils.Logger
 
 class Runner {
 
     boolean verbose
+    Logger log
 
     String projectName
 
     Runner(boolean verbose) {
         this.verbose = verbose
+        log = new Logger()
     }
 
     void execute(){
         // Create project folder
         projectName = Asker.requiredAsk('Specify your project name')
-        if (verbose) println "A new project named [$projectName] will be created"
+        if (verbose) log.indentLog("A new project named [$projectName] will be created")
+
+        if (!createDirectory(projectName))
+            log.error("Unable to create project folder [$projectName]. This is often due to a folder already named like that")
+
+
+    }
+
+    /***********************/
+    /** UTILITIES METHODS **/
+    /***********************/
+
+    private boolean createDirectory(String path){
+        File dir = new File(path)
+        return dir.mkdirs()
     }
 }
---
+++ src/main/groovy/org/abnt/stripes/generator/utils/Logger.groovy
@@ -0,0 +1,15 @@
+package org.abnt.stripes.generator.utils
+
+class Logger {
+
+    void error(String message){
+        println('AN ERROR OCCURS :')
+        indentLog(message)
+        println('The program will exit')
+        System.exit(1)
+    }
+
+    void indentLog(String message){
+        println """| $message \n"""
+    }
+}
